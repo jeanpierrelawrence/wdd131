3e00515ebf67b0fc59f9a53a5f301927c05debd8
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-04-10 22:41:41 +0200

    create page functionality finished

--- project/scripts/create.js
+++ project/scripts/create.js
@@ -1,3 +1,11 @@
+if (localStorage.getItem("showSuccess")) {
+    document.getElementById("success").classList.add("visible");
+    localStorage.removeItem("showSuccess")
+    setTimeout(() => {
+        document.getElementById("success").classList.remove("visible");
+    }, 3000);
+}
+
 const fieldsets = document.querySelectorAll('fieldset');
 const buttons = document.querySelectorAll('.buttons button');
 let currentStep = 0;
@@ -35,3 +43,35 @@ buttons.forEach(button => {
 });
 
 updateStep();
+
+/* FORM LOGIC */
+
+let memoForm = document.querySelector("#form");
+
+memoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let memoName = document.getElementById("name").value
+    let memoSummary = document.getElementById("summary").value
+    let memoCode = document.getElementById("code").value
+
+    const memoData = {
+        name: memoName,
+        summary: memoSummary,
+        code: memoCode,
+        id: Date.now()
+    }
+
+    saveToLocalStorage(memoData);
+    localStorage.setItem("showSuccess", "true");
+
+    window.location.href = "create.html"
+    
+})
+
+function saveToLocalStorage(newEntry) {
+    let library = JSON.parse(localStorage.getItem("memo_library")) || [];
+    library.push(newEntry);
+
+    localStorage.setItem("memo_library", JSON.stringify(library));
+}
